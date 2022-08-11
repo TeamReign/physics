c4510b39daa5fa778cc5ee86481e2590032a42e8
TeamReign <110091356+TeamReign@users.noreply.github.com>
2022-08-12 00:03:54 +0530

    Physics

---
+++ questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which of the following laws is associated with the heating effect of electric current?",
+    answer: "Joule's law",
+    options: [
+      "Joule's law",
+      "Ohm's law",
+      "Faraday's law",
+      "Newton's law"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The work done is zero if there is no?",
+    answer: "Displacement",
+    options: [
+      "Velocity",
+      "Displacement",
+      "Momentum",
+      "Power"
+    ]
+  },
+    {
+    numb: 3,
+    question: "An electric iron of 750 W is used for 8 hours per day. Then the energy consumed in one day by the iron is: ",
+    answer: "6 units",
+    options: [
+      "60 units",
+      "0. 6 units",
+      "600 units",
+      "6 units"
+    ]
+  },
+    {
+    numb: 4,
+    question: "A car, while moving, is facing a wind from the opposite direction. The work done by the wind on the car will be . . . ",
+    answer: "Negative",
+    options: [
+      "Infinity",
+      "Negative",
+      "Zero",
+      "Positive"
+    ]
+  },
+    {
+    numb: 5,
+    question: "The relation between potential difference (V) and current (I) was discovered by:",
+    answer: "Ohm",
+    options: [
+      "Tesla",
+      "Newton",
+      "Ohm",
+      "Pascal"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
